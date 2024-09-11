b82322faa947e8850d0a95c7a4f470c82e8743fe
Akash Jambulkar <97665573+Akash-Jambulkar@users.noreply.github.com>
2024-09-12 00:33:27 +0530

    Add files via upload

---
+++ java01.java
@@ -0,0 +1,8 @@
+package java00; //declaring package java00
+
+public class java01 { //declaring class java01
+	public static void main(String[] args) { //declaring main method
+		System.out.println("Hello, Java!"); //write 
+	}
+}
+
