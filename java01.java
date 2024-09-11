87f3e2f256528ca96381a12c0195dfcf6aba0fa5
Akash Jambulkar <97665573+Akash-Jambulkar@users.noreply.github.com>
2024-09-12 00:36:01 +0530

    Update MyName.java

--- java01.java
+++ java01.java
@@ -2,7 +2,7 @@ package java00; //declaring package java00
 
 public class java01 { //declaring class java01
 	public static void main(String[] args) { //declaring main method
-		System.out.println("Hello, Java!"); //write 
+		System.out.println("Hello, My name is Akash Jambulkar"); //write 
 	}
 }
 
