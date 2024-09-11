304fc86cdbeebf8e5d2bed0202caf6a37b3368c5
Akash Jambulkar <97665573+Akash-Jambulkar@users.noreply.github.com>
2024-09-12 00:35:10 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package java00; //declaring package java00
+
+public class HelloWorld { //declaring class HelloWorld
+	public static void main(String[] args) { //declaring main method
+		System.out.println("Hello, Java!"); //write 
+	}
+}
+
