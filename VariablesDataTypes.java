b44b232f128dc485c1f31f59de914df497411f63
Akash Jambulkar <97665573+Akash-Jambulkar@users.noreply.github.com>
2024-09-14 17:20:13 +0530

    Variable and data types

---
+++ VariablesDataTypes.java
@@ -0,0 +1,26 @@
+package java00;
+
+public class VariablesDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//initializing variables : 
+		int age = 25;
+		float height = 5.9f;
+		boolean isStudent = true;
+		char grade = 'A';
+		
+		System.out.println("Boolean Type : "+isStudent);
+		System.out.println("Integer Type : "+age);
+		System.out.println("Float Type : "+height);
+		System.out.println("Character Type : "+grade);
+		
+		//TypeCasting
+		
+		//implicit casting : Short to Long 
+		//explicit casting : long to short 
+		
+		//ternary operator : expression? value1 : value2
+	}
+
+}
